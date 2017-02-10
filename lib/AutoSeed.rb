9acf235627ba6bce4ebc7a21e859e4c82171f390
Yarbrough, Eric <enyarbrough@gmail.com>
2017-02-09 19:29:53 -0500

    change name to railtie

--- lib/AutoSeed.rb
+++ lib/AutoSeed.rb
@@ -1,6 +1,6 @@
 # require "AutoSeed/version"
  require "AutoSeed/bar"
- require "AutoSeed/rail_tie" if defined?(Rails)
+ require "AutoSeed/railtie" if defined?(Rails)
 
   class Foo
     def self.bar
