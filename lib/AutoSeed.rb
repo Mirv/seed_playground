44ebe623af7548ec22f5229afff1741cbad8795e
Yarbrough, Eric <eric.yarbrough@sage.com>
2017-02-13 15:48:37 -0500

    add json

--- lib/AutoSeed.rb
+++ lib/AutoSeed.rb
@@ -5,22 +5,24 @@
     def self.bar (params = {})
       Rails.application.eager_load!
 
-      included_models = params['INCLUDE'].split(',')
-
       # Filter the list of models from Active Record
-      models = ActiveRecord::Base.descendants.map { |model|
-        model.name } & included_models
+      models = ActiveRecord::Base.descendants.map { |model| model.name }
+
+      models = models & params['INCLUDE'].split(',') if params['INCLUDE']
+      models = models | params['REJECT'].split(',') if params['REJECT']
 
       models.each do | model_name |
 
       # Print the model creation method
-      puts "[" + model_name.classify.constantize.all.map { |model|
-         model.attributes.map { |key, attribute|
-           "#{key}: '#{attribute}'"
-         }.join(", ")
-      }.join(",\n") + "]"
+        puts "[{" + model_name.classify.constantize.all.map { |model|
+          model.attributes.reject{ |key| key == "id" }.map { |key, attribute|
+            key == "id" ? nil : "#{key}: '#{attribute}'"
+          }.join(", ")
+        }.join("},\n{")
+        + "}].each { |attributes| \n"
+        + model_name + ".find_or_create_by(attributes) }"
 
-        end
+      end
 
     end
   end
