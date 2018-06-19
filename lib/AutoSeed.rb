37e2aba406a821f556589e449cac7f27c87e2adb
Mirv <reski.raptor@gmail.com>
2018-06-19 22:53:11 +0000

    reformat of generate do loop, 2 defaults, create will report errors now

--- lib/AutoSeed.rb
+++ lib/AutoSeed.rb
@@ -1,28 +1,22 @@
- require "AutoSeed/railtie" if defined?(Rails)
+require "AutoSeed/railtie" if defined?(Rails)
 
-  class AutoSeed
-    def self.generate (params = {})
-      Rails.application.eager_load!
+class AutoSeed2
+  def self.generate (params = {})
+    Rails.application.eager_load!                   # Ensures models loaded
+    reject_us = ["id", "created_at", "updated_at"]  # Attributes to filter
+    insert_string = "Blah1234"
 
-      # Filter the list of models from Active Record
-      models = ActiveRecord::Base.descendants.map { |model| model.name }
+    # Filter the list of models from Active Record based on options in params
+    models = ActiveRecord::Base.descendants.map { |model| model.name }
+    models = models & params['INCLUDE'].split(',') if params['INCLUDE']
+    models = models | params['REJECT'].split(',') if params['REJECT']
 
-      models = models & params['INCLUDE'].split(',') if params['INCLUDE']
-      models = models | params['REJECT'].split(',') if params['REJECT']
-
-      models.each do | model_name |
-
-      # Print the model creation method
-        puts "[{" + model_name.classify.constantize.all.map { |model|
-          model.attributes.reject{ |key| key == "id" }.map { |key, attribute|
-            key == "id" ? nil : "#{key}: '#{attribute}'"
-          }.join(", ")
-        }.join("},\n{")
-        + "}].each { |attributes| \n"
-        + model_name + ".find_or_create_by(attributes) }"
-
-      end
-
+    models.each do | model_name |
+      model = model_name.classify.constantize        # setup calls model
+      attribs = model.column_names - reject_us       # drop any columns 
+      model_params = attribs.map { |x| [x, insert_string] }.to_h # setup hash of columns
+      result = model.find_or_create_by!(model_params)
+      puts result
     end
   end
-
+end
