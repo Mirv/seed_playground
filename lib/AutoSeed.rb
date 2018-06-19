79d93e288ec6b2fc30fcbb706c113e0a413e382b
Mirv <reski.raptor@gmail.com>
2018-06-19 22:54:39 +0000

    another comment & spacing

--- lib/AutoSeed.rb
+++ lib/AutoSeed.rb
@@ -3,8 +3,8 @@ require "AutoSeed/railtie" if defined?(Rails)
 class AutoSeed2
   def self.generate (params = {})
     Rails.application.eager_load!                   # Ensures models loaded
-    reject_us = ["id", "created_at", "updated_at"]  # Attributes to filter
-    insert_string = "Blah1234"
+    reject_attributes = ["id", "created_at", "updated_at"]  # Attributes to filter
+    insert_string = "Blah1234"                      # default string in attributes
 
     # Filter the list of models from Active Record based on options in params
     models = ActiveRecord::Base.descendants.map { |model| model.name }
@@ -12,11 +12,11 @@ class AutoSeed2
     models = models | params['REJECT'].split(',') if params['REJECT']
 
     models.each do | model_name |
-      model = model_name.classify.constantize        # setup calls model
-      attribs = model.column_names - reject_us       # drop any columns 
+      model = model_name.classify.constantize               # setup calls model
+      attribs = model.column_names - reject_attributes      # drop any columns 
       model_params = attribs.map { |x| [x, insert_string] }.to_h # setup hash of columns
-      result = model.find_or_create_by!(model_params)
-      puts result
+      result = model.find_or_create_by!(model_params)       # create & record result 
+      puts result                                           # output results
     end
   end
 end
