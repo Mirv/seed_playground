f1457cf73e4de958c807d3389875a7e309d21f47
Mirv <reski.raptor@gmail.com>
2018-06-25 17:46:14 +0000

    extracting file signature method

--- lib/AutoSeed.rb
+++ lib/AutoSeed.rb
@@ -25,31 +25,31 @@ class AutoSeed
   end
 end
 
-# TODO - add in error handling - done
-# TODO - add in ActiveRecord::Migration.check_pending! check
-# TODO - add a block for finger print of the filesize to be added to name
+# # TODO - add in error handling - done
+# # TODO - add in ActiveRecord::Migration.check_pending! check
+# # TODO - add a block for finger print of the filesize to be added to name
 
-class AutoSeed2
-  def self.generate (params = {})
-    raise Error.new("Migrations Pending") if ActiveRecord::Migration.check_pending!
-    Rails.application.eager_load!                   # Ensures models loaded
-    reject_attribs = ["id", "created_at", "updated_at"]  # Attributes to filter
-    insert_string = "Blah1234"                      # default string in attributes
+# class AutoSeed2
+#   def self.generate (params = {})
+#     raise Error.new("Migrations Pending") if ActiveRecord::Migration.check_pending!
+#     Rails.application.eager_load!                   # Ensures models loaded
+#     reject_attribs = ["id", "created_at", "updated_at"]  # Attributes to filter
+#     insert_string = "Blah1234"                      # default string in attributes
 
-    # Filter the list of models from Active Record based on options in params
-    models = ActiveRecord::Base.descendants.map { |model| model.name }
-    models = models & params['INCLUDE'].split(',') if params['INCLUDE']
-    models = models | params['REJECT'].split(',') if params['REJECT']
+#     # Filter the list of models from Active Record based on options in params
+#     models = ActiveRecord::Base.descendants.map { |model| model.name }
+#     models = models & params['INCLUDE'].split(',') if params['INCLUDE']
+#     models = models | params['REJECT'].split(',') if params['REJECT']
 
-    models.each do | model_name |
-      model = model_name.classify.constantize             # setup model to call
-      attribs = model.column_names - reject_attribs       # drop extra db columns 
-      attribs = attribs.map {|x| [x, insert_string]}.to_h # setup hash of columns
-      result = model.find_or_create_by!(attribs)          # save error msgs 
-      # puts result                                         # output results
-    end
-  end
-end
+#     models.each do | model_name |
+#       model = model_name.classify.constantize             # setup model to call
+#       attribs = model.column_names - reject_attribs       # drop extra db columns 
+#       attribs = attribs.map {|x| [x, insert_string]}.to_h # setup hash of columns
+#       result = model.find_or_create_by!(attribs)          # save error msgs 
+#       # puts result                                         # output results
+#     end
+#   end
+# end
 
 # add in randomizer or this slows system too much?
 
--- lib/test_run.rb
+++ lib/test_run.rb
@@ -1,13 +1,13 @@
 require 'benchmark'
 require 'AutoSeed'    # TODO - need to dynamically handle this?
+# require 'AutoSeed2'
+load '../lib/AutoSeed2.rb'
 load '../lib/AutoFileLogger.rb' # TODO - need to figure out why can't require this?
 
 class TestRun 
-  
-  # TODO - class pulls @test array to handle storage
-  def self.create_logfile(target)
-    # Task create the name of the log file based on the class tested
-    name = @tests[target].to_s.split(" ").last.split('.').first
+  # Send attributes to the eigenClass  
+  class << self
+    attr_accessor :tests
   end
   
   @tests = {
@@ -16,11 +16,6 @@ class TestRun
     three: AutoSeed3.method(:generate)
   }
   @logging = AutoFileLogger.new(folder: "benchmark")
-
-  # Send both attributes to the eigenClass  
-  class << self
-    attr_accessor :tests
-  end
 
   def self.test_run
     time_test
@@ -31,12 +26,13 @@ class TestRun
       t = timer do
         y.call
       end 
-      test = @tests[x].to_s.split(" ").last.split('.').first
+      test = @logging.discover_log_name(@tests[x], y.name)
+      puts "File written to: #{test}.log"
       @logging.logger(test)   # TODO - figure out why I have to initialize this explicitly
       write_test_result(test,t)
     end
   end
-  
+
   # Formatting on the messages
   def self.write_test_result(test, time)
     @logging.write_log(test, "#{time.real.round(5)} - #{self.name}")
