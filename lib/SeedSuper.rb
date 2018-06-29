45a706ce579ba11d8d6e0f2e0d380c769f84e143
Mirv <reski.raptor@gmail.com>
2018-06-29 06:47:00 +0000

    commment rearrange

--- lib/SeedSuper.rb
+++ lib/SeedSuper.rb
@@ -5,13 +5,10 @@ class SeedSuper
     # If migrations pending - why bother doing anything else?
     raise Error.new("Migrations Pending") if ActiveRecord::Migration.check_pending!
     models = ModelDiscovery.find_models(params['ONLY'], params['EXCLUDE'])
- 
-    # TODO - this belongs in AutoSeed 2 & 3 but not 1 ... which means they need a sub-superClass to implement it
-    # TODO - this chunk could be one method - might have to in order to implement inheriting class
     models.each do | model_name |
-      # STUB for the inheriting class to run here
       # ensure positive integer passes to next
       reps = params['REPS'].present? ? params['REPS'].to_i : 1
+      # STUB for the inheriting class to run here
       generate_seeding(model_name, reps)
     end
   end
