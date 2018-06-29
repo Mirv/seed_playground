a72566edbfc0518903b851321e0d5b31085ee885
Mirv <reski.raptor@gmail.com>
2018-06-29 07:01:37 +0000

    if rearranged

--- lib/model_discovery.rb
+++ lib/model_discovery.rb
@@ -5,7 +5,7 @@ class ModelDiscovery
     Rails.application.eager_load!                     # Ensures models loaded
     if(only_models.nil?)
       reject_models = ["ActiveRecord::SchemaMigration"] # problematic model
-      reject_models << ignore_models.split(',') if ignore_models
+      reject_models << ignore_models.split(',').trim if ignore_models
       models = ActiveRecord::Base.descendants.map(&:name) - reject_models.flatten 
     else
       # lower priority & versus && or and ensures models only keeps good ones
