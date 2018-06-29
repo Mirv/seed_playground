0ed1fb3108d2137adb2839f9aa2a2c9bfdf3c201
Mirv <reski.raptor@gmail.com>
2018-06-29 06:57:08 +0000

    strip to allow params to have spaces

--- lib/model_discovery.rb
+++ lib/model_discovery.rb
@@ -3,11 +3,14 @@
 class ModelDiscovery
   def self.find_models(only_models, ignore_models)
     Rails.application.eager_load!                     # Ensures models loaded
-    reject_models = ["ActiveRecord::SchemaMigration"] # problematic model
-    reject_models << ignore_models.split(',') if ignore_models
-    models = ActiveRecord::Base.descendants.map(&:name) - reject_models.flatten 
-    # lower priority & versus && or and ensures models only keeps good ones
-    models = models & only_models.split(',') if only_models
+    if(only_models.nil?)
+      reject_models = ["ActiveRecord::SchemaMigration"] # problematic model
+      reject_models << ignore_models.split(',') if ignore_models
+      models = ActiveRecord::Base.descendants.map(&:name) - reject_models.flatten 
+    else
+      # lower priority & versus && or and ensures models only keeps good ones
+      models = only_models.split(',').strip
+    end
     return models   # return required here currently
   end
 end
